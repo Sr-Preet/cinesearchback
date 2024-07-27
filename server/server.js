924b43a2db7c98543d0eabdf512ecbcb95e089f3
Sarpreet <Ssarpreet.786@gmail.com>
2024-07-27 19:15:59 -0400

    added cors

--- server/server.js
+++ server/server.js
@@ -4,10 +4,18 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
+
+// middleware
+const corsOptions = {
+  origin: "https://cinesearchfront.onrender.com", // frontend URI (ReactJS)
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 // serve client/build as static assets in production mode
 if (process.env.NODE_ENV === 'production') {
